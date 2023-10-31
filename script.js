b01af9ae4790e52eaf7869e8d28fe1fe8f2990a7
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-10-31 05:23:55 -0700

    create plan

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+/*
+game engine
+    player turn
+    while game not over
+    start over
+    winner/loser message
+
+player
+    name
+    symbol
+
+gameboard
+    array - 3 states empty, x, y
+    game over
+    render
+
+later - computer
+    pick random spots
+
+*/
 alert('hii');
