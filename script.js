55dacbde2caf4a85ce11d309bce19c2d9d2118b3
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-11-06 03:57:13 -0800

    chatgpt make private

--- script.js
+++ script.js
@@ -6,16 +6,26 @@ const GAME_STATUS = {
 };
 
 class Gameboard {
-  initialize() {
-    this.gameBoardArray = [
+  #gameBoardArray;
+
+  constructor() {
+    this.#gameBoardArray = this.#emptyBoard();
+  }
+
+  #emptyBoard() {
+    return [
       [' ', ' ', ' '],
       [' ', ' ', ' '],
       [' ', ' ', ' ']
     ];
+  }
+
+  initialize() {
+    this.#gameBoardArray = this.#emptyBoard();
     this.render();
   }
 
-  getBox(text, i, j) {
+  #getBox(text, i, j) {
     const box = document.createElement('div');
     box.textContent = text;
     box.style.height = '50px';
@@ -34,47 +44,45 @@ class Gameboard {
       const div = document.createElement('div');
       div.style.display = 'flex';
       for (let j = 0; j < 3; j++) {
-        div.appendChild(this.getBox(this.gameBoardArray[i][j], i, j));
+        div.appendChild(this.#getBox(this.#gameBoardArray[i][j], i, j));
       }
       gameBoardElement.appendChild(div);
     }
   }
 
   markPosition(i, j, currentPlayerMark) {
-    if (this.gameBoardArray[i][j] !== ' ') {
+    if (this.#gameBoardArray[i][j] !== ' ') {
       return false;
     }
-    this.gameBoardArray[i][j] = currentPlayerMark;
+    this.#gameBoardArray[i][j] = currentPlayerMark;
     this.render();
     return true;
   }
 
-  gameIsADraw() {
-    return this.gameBoardArray.flat().every(cell => cell !== ' ');
+  #gameIsADraw() {
+    return this.#gameBoardArray.flat().every(cell => cell !== ' ');
   }
 
-  checkWinner(playerMark) {
-    // Check rows, columns and diagonals
+  #checkWinner(playerMark) {
     const lines = [
-      ...this.gameBoardArray, // Rows
-      ...this.gameBoardArray[0].map((_, colIndex) =>
-        this.gameBoardArray.map(row => row[colIndex])
-      ), // Columns
-      [0, 1, 2].map(index => this.gameBoardArray[index][index]), // Main diagonal
-      [0, 1, 2].map(index => this.gameBoardArray[index][2 - index]) // Anti diagonal
+      ...this.#gameBoardArray,
+      ...this.#gameBoardArray[0].map((_, colIndex) =>
+        this.#gameBoardArray.map(row => row[colIndex])
+      ),
+      [0, 1, 2].map(index => this.#gameBoardArray[index][index]),
+      [0, 1, 2].map(index => this.#gameBoardArray[index][2 - index])
     ];
-
     return lines.some(line => line.every(cell => cell === playerMark));
   }
 
   isGameOver() {
-    if (this.checkWinner('X')) {
+    if (this.#checkWinner('X')) {
       return GAME_STATUS.X_WINS;
     }
-    if (this.checkWinner('O')) {
+    if (this.#checkWinner('O')) {
       return GAME_STATUS.O_WINS;
     }
-    if (this.gameIsADraw()) {
+    if (this.#gameIsADraw()) {
       return GAME_STATUS.DRAW;
     }
     return GAME_STATUS.NOT_A_DRAW;
@@ -82,74 +90,91 @@ class Gameboard {
 }
 
 class GameManager {
+  #gameBoard;
+  #isXTurn;
+  #isGameOver;
+  #gameInfo;
+  #gameBoardElement;
+  #playerX;
+  #playerO;
+
   constructor(gameBoard) {
-    this.gameBoard = gameBoard;
-    this.isXTurn = true;
-    this.isGameOver = false;
-    this.gameInfo = document.querySelector('.game-info');
-    this.gameBoardElement = document.querySelector('.game-board');
-    this.playerX = document.querySelector('.playerX');
-    this.playerO = document.querySelector('.playerO');
+    this.#gameBoard = gameBoard;
+    this.#isXTurn = true;
+    this.#isGameOver = false;
+    this.#gameInfo = document.querySelector('.game-info');
+    this.#gameBoardElement = document.querySelector('.game-board');
+    this.#playerX = document.querySelector('.playerX');
+    this.#playerO = document.querySelector('.playerO');
+    this.initialize();
   }
 
-  addRestartButton() {
+  #addRestartButton() {
     const button = document.createElement('button');
     button.textContent = 'Start a new game';
-    this.gameInfo.appendChild(button);
+    this.#gameInfo.appendChild(button);
     button.addEventListener('click', () => {
-      this.gameInfo.removeChild(button);
-      this.gameBoardElement.classList.remove('disabled');
+      this.#gameInfo.removeChild(button);
+      this.#gameBoardElement.classList.remove('disabled');
       this.initialize();
     });
   }
 
-  checkGameStatus() {
-    const status = this.gameBoard.isGameOver();
+  #checkGameStatus() {
+    const status = this.#gameBoard.isGameOver();
     if (status !== GAME_STATUS.NOT_A_DRAW) {
-      this.isGameOver = true;
+      this.#isGameOver = true;
       let gameOverMessage;
       switch (status) {
         case GAME_STATUS.DRAW:
-          gameOverMessage = `${this.playerX.value} tied with ${this.playerO.value}`;
+          gameOverMessage = `${this.#playerX.value} tied with ${
+            this.#playerO.value
+          }`;
           break;
         case GAME_STATUS.X_WINS:
-          gameOverMessage = `${this.playerX.value} defeated ${this.playerO.value}`;
+          gameOverMessage = `${this.#playerX.value} defeated ${
+            this.#playerO.value
+          }`;
           break;
         case GAME_STATUS.O_WINS:
-          gameOverMessage = `${this.playerO.value} defeated ${this.playerX.value}`;
+          gameOverMessage = `${this.#playerO.value} defeated ${
+            this.#playerX.value
+          }`;
           break;
       }
 
       const messageDiv = document.createElement('p');
       messageDiv.textContent = gameOverMessage;
-      this.gameBoardElement.appendChild(messageDiv);
-      this.gameBoardElement.classList.add('disabled');
-      this.addRestartButton();
+      this.#gameBoardElement.appendChild(messageDiv);
+      this.#gameBoardElement.classList.add('disabled');
+      this.#addRestartButton();
     }
   }
 
   initialize() {
-    this.gameBoard.initialize();
-    this.isXTurn = true;
-    this.isGameOver = false;
+    this.#gameBoard.initialize();
+    this.#isXTurn = true;
+    this.#isGameOver = false;
   }
 
   getCurrentPlayerMark() {
-    return this.isXTurn ? 'X' : 'O';
+    return this.#isXTurn ? 'X' : 'O';
   }
 
   changePlayer() {
-    if (!this.isGameOver) {
-      this.isXTurn = !this.isXTurn;
-      this.checkGameStatus();
-    }
+    if (this.#isGameOver) return;
+    this.#isXTurn = !this.#isXTurn;
+    this.#checkGameStatus();
+  }
+
+  get isGameOver() {
+    return this.#isGameOver;
   }
 }
 
 // Initialization code
 const theGameboard = new Gameboard();
 const theGameManager = new GameManager(theGameboard);
-theGameManager.initialize();
 
 document.querySelector('.game-board').addEventListener('click', event => {
   if (event.target.classList.contains('box') && !theGameManager.isGameOver) {
